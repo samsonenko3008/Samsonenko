12b089c67beb13c0bc836210a24d10465823080b
samsonenko3008 <149273438+samsonenko3008@users.noreply.github.com>
2023-12-07 23:43:47 +0300

    Update task_2.py

--- Лабораторная работа№4/task_2.py
+++ Лабораторная работа№4/task_2.py
@@ -1,20 +1,23 @@
 # TODO импортировать необходимые молули
-
+import csv
+import json
 
 INPUT_FILENAME = "input.csv"
 OUTPUT_FILENAME = "output.json"
 
 
-def task() -> None:
-    ...  # TODO считать содержимое csv файла
+def task():
+    with open(INPUT_FILENAME) as c:
+        reader = csv.DictReader(c, delimiter=",", quotechar="\n")
+        row = list(reader)  # TODO считать содержимое csv файла
 
-    ...  # TODO Сериализовать в файл с отступами равными 4
+    with open(OUTPUT_FILENAME, "w") as f:
+        json.dump(row, f, indent=4)  # TODO Сериализовать в файл с отступами равными 4
 
 
-if __name__ == '__main__':
-    # Нужно для проверки
-    task()
 
-    with open(OUTPUT_FILENAME) as output_f:
-        for line in output_f:
-            print(line, end="")
+task()
+
+with open(OUTPUT_FILENAME) as output_f:
+    for line in output_f:
+        print(line, end="")
